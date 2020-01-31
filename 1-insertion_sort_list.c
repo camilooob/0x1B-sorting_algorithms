6312ac3a0f0114a3ebe1e41aa46074eb085851f4
camilob.com <camilobaq@hotmail.com>
2020-01-31 02:00:20 +0000

    0 bubble ok work

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort algothm
+ * @size: size input
+ * @array: pointer input
+ * Return: No Return
+ */
+ void selection_sort(int *array, size_t size)
+ {
+	 hola hola mundo 
+ }
