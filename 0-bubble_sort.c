5429cd6d7f2474928dc8213b79f92d64e1702c70
Julian Villegas <julian.villegasplus@gmail.com>
2020-02-01 00:03:42 +0000

    [UPD]0-bubble_sort: improving betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,21 +6,21 @@
  * @array: pointer input
  * Return: No Return
  */
- void bubble_sort(int *array, size_t size)
- {
+void bubble_sort(int *array, size_t size)
+{
 	unsigned int i, j, swap;
 
-	for (i = 0; i < size -1; i++)
-	{	
+	for (i = 0; i < size - 1; i++)
+	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				swap = array[j];
-				array[j] = array[j+1];
-				array[j+1] = swap;
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
 				print_array(array, size);
-			}				
+			}
 		}
-	}	
- }
+	}
+}
