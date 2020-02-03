9fe2d05d3135b51adfb464250effd238854bb204
camilob.com <camilobaq@hotmail.com>
2020-02-03 18:13:35 +0000

    [Work] ok 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,55 +1,88 @@
 #include "sort.h"
 
+/**
+ * myswap - swaps two values
+ *
+ * @array: data sort input
+ * @firts: first
+ * @second: second
+ * @size: size
+ *
+ * Return: No Return
+ */
+void myswap(int *array, int firts, int second, int size)
+{
+	int tmp;
+
+	if (array[firts] != array[second])
+	{
+		tmp = array[firts];
+		array[firts] = array[second];
+		array[second] = tmp;
+		print_array(array, size);
+	}
+}
+
+/**
+ * part - part to a pivot
+ *
+ * @array: data sort input 
+ * @left: left
+ * @right: right
+ * @size: size input
+ *
+ * Return: New pivote
+ */
 int part(int *array, int left, int right, size_t size)
 {
-	int i = left , pivote = left, num_pivote = array[right], tmp;
+	int i = left, j, pivot  = array[right];
 
-	for (pivote = left; pivote <= right; pivote++)
+	for (j = left; j <= right; j++)
 	{
-		if (array[pivote] < num_pivote)
+		if (array[j] < pivot)
 		{
-			if (array[i] != array[pivote])
-			{
-				tmp = array[i];
-				array[i] = array[pivote];
-				array[pivote] = tmp;
-				print_array(array, size);
-			}
+			myswap(array, i, j, size);
 			i++;
 		}
 	}
-	tmp = array[i];
-	array[i] = array[pivote];
-	array[pivote] = tmp;
-	print_array(array, size);
-
+	myswap(array, i, right, size);
 
 	return (i);
 }
 
-
+/**
+ * quicksort -  quick sort algorithm
+ *
+ * @array: data to sort
+ * @left: Left pivote
+ * @right: right pivote
+ * @size: size input
+ * Return: No Return
+ */
 void myquicksort(int *array, int left, int right, size_t size)
 {
-    int pivote;
-        if (left < right)
-        {
-            pivote = part(array, left, right, size);
-            myquicksort(array, left, pivote - 1, size);
-            myquicksort(array, pivote + 1, right, size);
-        }
+	int pivote;
+
+	if (left < right)
+	{
+		pivote = part(array, left, right, size);
+		myquicksort(array, left, pivote - 1, size);
+		myquicksort(array, pivote + 1, right, size);
+	}
 }
 
 /**
- * quick sort - quick sort algorthms
+ * quick_sort -  quick sort algorithm
+ *
+ * @array: sort data
  * @size: size input
- * @array: pointer input
+ *
  * Return: No Return
  */
- void quick_sort(int *array, size_t size)
- {
-     if (size < 2)
-         {
-             return;
-         }
-     myquicksort(array, 0, size - 1, size);
- }
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+
+	myquicksort(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -21,4 +21,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int part(int *array, int left, int right, size_t size);
+void myquicksort(int *array, int left, int right, size_t size);
+void myswap(int *array, int firts, int second, int size);
 #endif
