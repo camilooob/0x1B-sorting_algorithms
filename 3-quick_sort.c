64eaf668fa0edfe4b8fb299b4210b46fa437814d
camilob.com <camilobaq@hotmail.com>
2020-02-03 16:52:21 +0000

    [Almost work 3]

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,9 +1,37 @@
 #include "sort.h"
 
+int part(int *array, int left, int right, size_t size)
+{
+	int i = left , pivote = left, num_pivote = array[right], tmp;
+
+	for (pivote = left; pivote <= right; pivote++)
+	{
+		if (array[pivote] < num_pivote)
+		{
+			if (array[i] != array[pivote])
+			{
+				tmp = array[i];
+				array[i] = array[pivote];
+				array[pivote] = tmp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	tmp = array[i];
+	array[i] = array[pivote];
+	array[pivote] = tmp;
+	print_array(array, size);
+
+
+	return (i);
+}
+
+
 void myquicksort(int *array, int left, int right, size_t size)
 {
     int pivote;
-        if (left < right);
+        if (left < right)
         {
             pivote = part(array, left, right, size);
             myquicksort(array, left, pivote - 1, size);
