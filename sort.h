ae116440055dee2f437499bb9194265b5f237624
Julian Villegas <julian.villegasplus@gmail.com>
2020-02-04 19:33:21 +0000

    [UPD]sort.h: Adding prototype

--- sort.h
+++ sort.h
@@ -1,7 +1,11 @@
 #ifndef SORTING
 #define SORTING
+
+/* Built-in libraries */
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Structs */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,12 +19,17 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+/* Prototypes */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+
+/* Custom functions */
 int part(int *array, int left, int right, size_t size);
 void myquicksort(int *array, int left, int right, size_t size);
 void myswap(int *array, int firts, int second, int size);
